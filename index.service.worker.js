78248bc6dc6b59d962933beeacf5fc7e40402cd7
koenvaneijk <33032220+koenvaneijk@users.noreply.github.com>
2024-08-03 13:28:49 +0000

    Deploying to gh-pages from @ koenvaneijk/monsterpinball@714980dbfbb8407e473816024dda6a8a2ca3366a 🚀

--- index.service.worker.js
+++ index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1722677125|2300083";
+const CACHE_VERSION = "1722691717|2316965";
 const CACHE_PREFIX = "Monster Pinball-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
